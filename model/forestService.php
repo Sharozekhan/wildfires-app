f7019922901fff073f77f2174b06b816392e1c15
sharozekhan <sharoze.khan@hotmail.co.uk>
2023-12-12 17:38:11 +0000

    Added in a search bar functionality and error handling for database requests

--- model/forestService.php
+++ model/forestService.php
@@ -1,5 +1,5 @@
 <?php
-
+// Located in /model/forestService.php
 class forestService {
     private $db;
 
@@ -8,20 +8,44 @@ class forestService {
     }
 
     public function getForests() {
-        $sql = "SELECT DISTINCT NWCG_REPORTING_UNIT_NAME FROM Fires";
-        $stmt = $this->db->prepare($sql);
-        $stmt->execute();
-
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        try {
+            $sql = "SELECT DISTINCT NWCG_REPORTING_UNIT_NAME FROM Fires";
+            $stmt = $this->db->prepare($sql);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            // Handle the error
+            error_log("Database error: " . $e->getMessage());
+            return [];
+        }
     }
 
     public function getForestDetails($forestName) {
-        $sql = "SELECT FPA_ID, FIRE_NAME, DISCOVERY_DATE, STAT_CAUSE_DESCR FROM Fires WHERE NWCG_REPORTING_UNIT_NAME = :forestName";
-        $stmt = $this->db->prepare($sql);
-        $stmt->bindValue(':forestName', $forestName, PDO::PARAM_STR);
-        $stmt->execute();
+        try {
+            $sql = "SELECT FPA_ID, FIRE_NAME, DISCOVERY_DATE, STAT_CAUSE_DESCR FROM Fires WHERE NWCG_REPORTING_UNIT_NAME = :forestName";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindValue(':forestName', $forestName, PDO::PARAM_STR);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            // Handle the error
+            error_log("Database error: " . $e->getMessage());
+            return [];
+        }
+    }
 
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    public function searchForests($searchQuery) {
+        try {
+            $sql = "SELECT DISTINCT NWCG_REPORTING_UNIT_NAME FROM Fires WHERE NWCG_REPORTING_UNIT_NAME LIKE :searchQuery";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindValue(':searchQuery', '%' . $searchQuery . '%', PDO::PARAM_STR);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            // Handle the error
+            error_log("Database error: " . $e->getMessage());
+            return [];
+        }
     }
 }
 ?>
